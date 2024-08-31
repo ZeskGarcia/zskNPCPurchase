1bd776fb4e14d38633ffbb9798d0474f2ad7aedc
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-08-31 22:19:14 +0200

    Create Config.lua

---
+++ Shared/Config.lua
@@ -0,0 +1,13 @@
+Shared = {}
+
+Shared.Weapons = {
+  WEAPON_MICROSMG = { name = "Micro SMG", enabled = true, price = 0 },
+  WEAPON_MACHINEPISTOL = { name = "MachinePistol", enabled = true, price = 0 },
+  WEAPON_PISTOL = { name = "Pistola", enabled = true, price = 0 },
+  WEAPON_COMBATPDW = { name = "PDW", enabled = true, price = 0 },
+  WEAPON_DOUBLEACTION = { name = "Pistola de Doble Acción", enabled = true, price = 0 },
+  WEAPON_APPISTOL = { name = "Pistola AP", enabled = true, price = 0 },
+  WEAPON_PISTOL50 = { name = "Pistol .50", enabled = true, price = 0 }
+}
+
+Shared.Coords = vector3(237.3282, -761.944, 30.840)
